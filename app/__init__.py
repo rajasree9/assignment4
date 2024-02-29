da6dd736fc00af65e953e54c6a4ad848ac548c2e
rajasree9 <rt484@njit.edu>
2024-02-29 12:58:47 -0500

    added commands and plugins

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,37 @@
-class App:
-    @staticmethod
-    def start() -> None:
-        print("Hello World. Type 'exit' to exit.")
-        
-        while True:
-            user_input = input(">>> ")
-            if user_input.lower() == "exit":
-                print("Exiting...")
-                break
-            else:
-                # Here, you could add additional commands and their handling
-                print("Unknown command. Type 'exit' to exit.")
+import pkgutil
+import importlib
+from app.commands import CommandHandler
+from app.commands import Command
+from app.plugins.menu import MenuCommand
 
-        
+class App:
+    def __init__(self): # Constructor
+        self.command_handler = CommandHandler()
+        menu_instance = MenuCommand(self.command_handler)
+
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                            if plugin_name == 'menu':
+                                menu_instance = MenuCommand(self.command_handler)
+                                self.command_handler.register_command(plugin_name, menu_instance)
+                            else:
+                                self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
+
+    def start(self):
+        # Register commands here
+        self.load_plugins()
+        print("Type 'exit' to exit.")
+        while True:  #REPL Read, Evaluate, Print, Loop
+            self.command_handler.execute_command(input(">>> ").strip())
+
+
---
+++ app/plugins/welcome/__init__.py
@@ -0,0 +1,6 @@
+from app.commands import Command
+
+
+class welcomeCommand(Command):
+    def execute(self):
+        print("welcome to this class")
